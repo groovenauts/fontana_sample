b2ebda0e53940aac55d391e3b59a0655b5641a2e
akima <akm2000@gmail.com>
2013-08-23 19:50:39 +0900

    モデルのテストとlibgssを使ったテストを分離して実行できるようにしました。 bundle exec rspec spec/models だけだと高速で実行可能です。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,6 @@ require 'fontana_client_support'
 
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each {|f| require f}
 
-if ENV['SYNC_DIRECTLY'] =~ /yes|on|true/i
-  Fontana::CommandUtils::system!("rake deploy:sync:update")
-end
-
 RSpec.configure do |config|
 
   # iOS開発環境が整っていない場合、SSLで接続する https://sandbox.itunes.apple.com/verifyreceipt が
