1788c9b9f5912f6f361f884e3e3be0c613851f4a
akima <akm2000@gmail.com>
2013-10-17 12:23:31 +0900

    [#58940902] 現象を再現できました。問題はシングルクォーテーションです。

--- spec/scripts/dictionary_with_array_input_spec.rb
+++ spec/scripts/dictionary_with_array_input_spec.rb
@@ -3,35 +3,53 @@ require File.expand_path('../spec_scripts_helper', __FILE__)
 
 describe "Dictionary with Array Input" do
 
-  let(:network){ new_network("1000007").tap(&:login!) } # HPが1の人
+  fixtures "simple"
+  before(:all) do
+    ClientRelease.delete_all
+    FactoryGirl.create(:client_release01)
+  end
+
+  let(:network){ new_network.tap(&:login!) } # HPが1の人
   let(:request){ network.new_action_request }
 
   inputs = [
-    []
-    [null]
-    [0]
-    [1]
-    [1000]
-    ["0"]
-    ["1"]
-    ["1000"]
-    [null, null]
-    [0, 0]
-    [1, 1]
-    [1000, 1000]
-    ["0", "0"]
-    ["1", "1"]
-    ["1000", "1000"]
+    []                                   , '[]',
+    [nil]                                , '[null]',
+    [0]                                  , '[0]',
+    [1]                                  , '[1]',
+    [1000]                               , '[1000]',
+    ["0"]                                , '["0"]',
+    ["1"]                                , '["1"]',
+    ["1000"]                             , '["1000"]',
+    [nil, nil]                           , '[null, null]',
+    [0, 0]                               , '[0, 0]',
+    [1, 1]                               , '[1, 1]',
+    [1000, 1000]                         , '[1000, 1000]',
+    ["0", "0"]                           , '["0", "0"]',
+    ["1", "1"]                           , '["1", "1"]',
+    ["1000", "1000"]                     , '["1000", "1000"]',
+    ["M01A01DM01",40000100,1]            , '["M01A01DM01",40000100,1]',
+    ["M01A01DM01","40000100","2"]        , '["M01A01DM01","40000100","2"]',
+    ["M01A01DM01",  "40000100",  "3"]    , '["M01A01DM01",  "40000100",  "3"]',
+    ['M01A01DM01',40000100,4]            , "['M01A01DM01',40000100,4]",
+    ['M01A01DM01','40000100','5']        , "['M01A01DM01','40000100','5']",
+    ['M01A01DM01',    '40000100',    '6'], "['M01A01DM01',    '40000100',    '6']",
   ]
 
-  inputs.each do |input|
-    it(input.inspect) do
+  Hash[*inputs].each do |input, expect|
+    it("raw #{input.inspect}") do
       request.execute("AppSeedTestScript", "get_dictionary_with_array_input1", input: input)
       request.send_request
       request.outputs.length.should == 1
-      request.outputs.first["result"].should == "output for #{input.inspect}"
+      request.outputs.first["result"].should == "output for #{expect}"
+    end
+
+    it("string #{input.inspect}") do
+      request.execute("AppSeedTestScript", "get_dictionary_with_array_input1", input: input.to_json)
+      request.send_request
+      request.outputs.length.should == 1
+      request.outputs.first["result"].should == "output for #{expect}"
     end
   end
 
 end
-
