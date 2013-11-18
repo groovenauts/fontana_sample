f45faf0c659233886812f07eacdeac2961d69a4d
akima <akm2000@gmail.com>
2013-11-18 12:33:57 +0900

    config/runtime.yml 改め config/fontana.yml を使用して実行時のディレクトリを取得するようにしました

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,19 +21,15 @@
 # Learn more: http://github.com/javan/whenever
 
 require 'tengine/support/yaml_with_erb'
-RUNTIME_CONFIG = YAML.load_file_with_erb(File.expand_path("../runtime.yml", __FILE__))
+RUNTIME_CONFIG = YAML.load_file_with_erb(File.expand_path("../fontana.yml", __FILE__))
 
-open("/tmp/config_schedule.log", "a") do |f|
-  f.puts("#{__FILE__}##{__LINE__}")
-  f.puts("Dir.pwd: " << Dir.pwd.inspect)
-  f.puts("\n  " << caller.join("\n  "))
-end
 
 # set :path instead of Whenever.path
 set :path, RUNTIME_CONFIG["gotool"]["path"]
+set :app_dir, RUNTIME_CONFIG["workspaces"]["runtime"]
 
 # set runner template instead of default template using runner_for_app
-job_type :runner, "cd :path && script/rails runner -e :environment :task :output"
+job_type :runner, "cd :path && script/rails runner -e :environment :app_dir/:task :output"
 
 every "*/10 * * * *" do # 10分おき
   runner "app/batches/calc_player_count_per_item_and_its_count.rb"
