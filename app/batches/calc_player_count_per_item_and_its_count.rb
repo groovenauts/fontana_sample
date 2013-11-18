798218230deda6ae8ff46240f2675123cd8b625a
akima <akm2000@gmail.com>
2013-11-18 12:47:53 +0900

    GameData#contentが期待していない状態の場合でもパスできるようにしました

--- app/batches/calc_player_count_per_item_and_its_count.rb
+++ app/batches/calc_player_count_per_item_and_its_count.rb
@@ -1,7 +1,7 @@
 
 result = {}
 GameData.all.each do |gd|
-  gd.content["items"].each do |item_code, item_count|
+  (gd.content["items"] || {}).each do |item_code, item_count|
     hash = result[item_code] ||= {}
     hash[item_count] ||= 0
     hash[item_count] += 1
