6c0846adb98e44dbd472801c5f7c5c96bedce01f
taigou <taigou@gmail.com>
2014-01-31 19:09:46 +0900

    [GSSレクチャー用の資料の作成]ハンズオン用のストアドスクリプトを作成していたが野口に渡すため一旦コミットします

--- app/scripts/car_parts.rb
+++ app/scripts/car_parts.rb
@@ -7,16 +7,20 @@ module CarParts
   def stocks(argh)
     car_cd      = argh[:car_cd]
     category_cd = argh[:category_cd]
+
     r = []
+
     if car_cd && category_cd
-      parts_in_this_category =
-        all(
-          name: "CarParts",
-          conditions: {
-            "car_cd" => car_cd,
-            "part_category_cd" => category_cd
-          }
-        )
+      parts_in_this_category = all(
+                                   name:       'CarParts',
+                                   conditions: { 'car_cd' => car_cd, 'part_category_cd' => category_cd }
+                               )
+
+      r = parts_in_this_category.map do |p|
+        c = count(name: 'Stock', conditions: { "part_cd" => p["part_cd"] })
+        n = first(name: 'Part' , conditions: { 'cd'      => p['part_cd'] })[:name]
+        "#{n}(#{c})"
+      end
     end
     r
   end
