b608a2caa0f9dc92dd98c7c83b15ae613e66d002
akima <akm2000@gmail.com>
2014-06-02 19:25:37 +0900

    テストをパスするように修正中・・・

--- spec/factory_girl_sample_spec.rb
+++ spec/factory_girl_sample_spec.rb
@@ -17,15 +17,15 @@ describe "factory_girl sample" do
 
         @async_action01 = FactoryGirl.create(:async_action01)
         @player01 = @async_action01.player
-        @network = new_network(@player01.player_id).login!
+        # @context = Fontana::Action::Context.new("test"){ @player01 }
       end
       it "same action_id type" do
-        r = network.new_async_action_request
-        r.async_status([1]).to_a.should == [{"id" => 1, "status" => "executing" }]
+        a = Fontana::Action::AsyncAction.peek(@player01, 1)
+        expect(a.id).to eq 1
+        expect(a.status).to eq "executing"
       end
       it "different action_id type" do
-        r = network.new_async_action_request
-        r.async_status(["1"]).to_a.should == [{"id" => "1", "status" => "not_found" , "message" => "not found for id: \"1\"" }]
+        expect(Fontana::Action::AsyncAction.peek(@player01, "1")).to be_nil
       end
     end
 
@@ -35,9 +35,12 @@ describe "factory_girl sample" do
         Fontana::Action::AsyncAction.delete_all
         @async_action02 = create(:async_action02)
         @player01 = @async_action02.player
-        @network = new_network(@player01.player_id).login!
       end
       it do
+        a = Fontana::Action::AsyncAction.peek(@player01, "2")
+        expect(a.id).to eq "2"
+        expect(a.status).to eq "executing"
+
         r = network.new_async_action_request
         r.async_status(["2"]).to_a.should == [{"result"=>1379990870, "id"=>"2"}]
       end
