da6c7aff6654fe75775e8c6a7b26a287c12b86ab
akima <akm2000@gmail.com>
2014-06-03 17:09:45 +0900

    テストをパスするように修正しました

--- spec/factory_girl_sample_spec.rb
+++ spec/factory_girl_sample_spec.rb
@@ -17,12 +17,12 @@ describe "factory_girl sample" do
 
         @async_action01 = FactoryGirl.create(:async_action01)
         @player01 = @async_action01.player
-        # @context = Fontana::Action::Context.new("test"){ @player01 }
       end
+
       it "same action_id type" do
         a = Fontana::Action::AsyncAction.peek(@player01, 1)
-        expect(a.id).to eq 1
-        expect(a.status).to eq "executing"
+        expect(a.action_id).to eq 1
+        expect(a.status).to eq :executing
       end
       it "different action_id type" do
         expect(Fontana::Action::AsyncAction.peek(@player01, "1")).to be_nil
@@ -36,18 +36,17 @@ describe "factory_girl sample" do
         @async_action02 = create(:async_action02)
         @player01 = @async_action02.player
       end
+      let(:cxt){ Fontana::Action::Context.new("test"){ @player01 } }
+
       it do
         a = Fontana::Action::AsyncAction.peek(@player01, "2")
-        expect(a.id).to eq "2"
-        expect(a.status).to eq "executing"
-
-        r = network.new_async_action_request
-        r.async_status(["2"]).to_a.should == [{"result"=>1379990870, "id"=>"2"}]
+        expect(a.action_id).to eq "2"
+        expect(a.status).to eq :success
+        expect(a.response).to eq({"result"=>1379990870, "id"=>"2"})
       end
 
       it "different action_id type" do
-        r = network.new_async_action_request
-        r.async_status([2]).to_a.should == [{"id" => 2, "status" => "not_found" , "message" => "not found for id: 2" }]
+        expect(cxt.async_call_results({'input_ids' => [2]}.to_json)).to eq [{id: 2, status: :not_found, message: "not found for id: 2" }]
       end
     end
 
